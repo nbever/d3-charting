e8a178c1e5ff15d9b1f64435c61fba9641ea8565
Hans Windhoff <hwindhoff@sovrn.com>
2017-07-26 17:14:22 -0600

    forma

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {render} from 'react-dom';
-import Chart from './charting/base_chart.js';
-import {LineSpec, LineNature} from './charting/line_nature.js';
-import {AxisSpec, AxisNature} from './charting/axis_nature.js';
-import {HoverAxisNature} from './charting/hover_axis_nature.js';
-import {CirclePointNature} from './charting/circle_point_nature.js';
-import {TrianglePointNature} from './charting/triangle_point_nature.js';
-import {PointSpec} from './charting/model/point_nature.js';
-import {BarNature, BarSpec} from './charting/bar_nature.js';
-import {StackedBarNature, StackedBarSpec} from './charting/stacked_bar_nature.js';
-import Header from './components/header.js';
-import ChartBlock from './components/chart_block.js';
-import {strings} from './utils/strings.js';
+import { render } from 'react-dom';
+import Chart from './charting/base_chart';
+import { LineSpec, LineNature } from './charting/line_nature';
+import { AxisSpec, AxisNature } from './charting/axis_nature';
+import { HoverAxisNature } from './charting/hover_axis_nature';
+import { CirclePointNature } from './charting/circle_point_nature';
+import { TrianglePointNature } from './charting/triangle_point_nature';
+import { PointSpec } from './charting/model/point_nature';
+import { BarNature, BarSpec } from './charting/bar_nature';
+import { StackedBarNature, StackedBarSpec } from './charting/stacked_bar_nature';
+import Header from './components/header';
+import ChartBlock from './components/chart_block';
+import { strings } from './utils/strings';
 
-require( './styles/base.scss' );
+require('./styles/base.scss');
 
-String.prototype.width = function(font) {
+String.prototype.width = function (font) {
   let o = $(`<div>${this}</div>`)
       .css({ position: 'absolute', float: 'left', 'white-space': 'nowrap', visibility: 'hidden', font })
-            .appendTo($('body')),
-      w = o.width();
+      .appendTo($('body')),
+    w = o.width();
 
   o.remove();
 
@@ -27,45 +27,61 @@ String.prototype.width = function(font) {
 };
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            data: {
+    this.state = {
+      data: {
         l1: {
-                    datapoints: [
+          datapoints: [
           ],
-                },
+        },
         l2: {
-                    datapoints: [
+          datapoints: [
           ],
         },
         l3: {
           datapoints: [],
-                },
-            },
-            natures: [
-                // new BarNature([
-                //     new BarSpec({key: 'l3', strokeWidth: 2, stroke: 'purple', fill: 'yellow', opacity: 0.4,
-                //         barWidth: 10, useGlobalScale: false, cursor: 'pointer'})
-                // ]),
-                new StackedBarNature([
-                    new StackedBarSpec({key: 'l3', strokeWidth: 2, stroke: 'purple', fill: 'darkgreen', opacity: 0.4,
-                        barWidth: 10, cursor: 'pointer'}),
-                    new StackedBarSpec({key: 'l2', strokeWidth: 2, stroke: 'black', fill: 'red', opacity: 0.4,
-                        barWidth: 10, cursor: 'pointer'})
-                ]),
-                new LineNature([
-                    new LineSpec({key: 'l1', color: 'green', thickness: 2.0}),
+        },
+      },
+      natures: [
+        new BarNature([
+          new BarSpec({ key: 'l3',
+            strokeWidth: 2,
+            stroke: 'purple',
+            fill: 'yellow',
+            opacity: 0.4,
+            barWidth: 10,
+            useGlobalScale: false,
+            cursor: 'pointer' }),
+        ]),
+        // new StackedBarNature([
+        //   new StackedBarSpec({ key: 'l3',
+        //     strokeWidth: 2,
+        //     stroke: 'purple',
+        //     fill: 'darkgreen',
+        //     opacity: 0.4,
+        //     barWidth: 10,
+        //     cursor: 'pointer' }),
+        //   new StackedBarSpec({ key: 'l2',
+        //     strokeWidth: 2,
+        //     stroke: 'black',
+        //     fill: 'red',
+        //     opacity: 0.4,
+        //     barWidth: 10,
+        //     cursor: 'pointer' }),
+        // ]),
+        new LineNature([
+          new LineSpec({ key: 'l1', color: 'green', thickness: 2.0 }),
           new LineSpec({ key: 'l2', color: 'blue' }),
-                ]),
-                new CirclePointNature([
+        ]),
+        new CirclePointNature([
           new PointSpec({ key: 'l1', stroke: 'red', fill: 'blue', radius: 10, opacity: 0.3, cursor: 'pointer' }),
-                ]),
-                new TrianglePointNature([
+        ]),
+        new TrianglePointNature([
           new PointSpec({ key: 'l2', stroke: 'green', fill: 'darkgray', radius: 3, cursor: 'pointer' }),
-                ]),
-                // new AxisNature(new AxisSpec({key: '', position: 'left', ticks: 3})),
+        ]),
+        // new AxisNature(new AxisSpec({key: '', position: 'left', ticks: 3})),
         new HoverAxisNature(new AxisSpec({ key: '',
           position: 'left',
           ticks: 1,
@@ -75,101 +91,101 @@ class App extends React.Component {
           labelFunction: tick => parseInt(tick).toFixed(2) })),
         new AxisNature(new AxisSpec({ key: '', position: 'bottom', ticks: 4, tickSizeOuter: 0 })),
       ],
-        };
-    }
+    };
+  }
 
-    buttonClicked() {
-        const newState = this.state;
-        const numbersToChange = Math.floor((Math.random() * 10) + 1);
+  buttonClicked() {
+    const newState = this.state;
+    const numbersToChange = Math.floor((Math.random() * 10) + 1);
 
-        const newData = {
+    const newData = {
       l1: {
-                datapoints: [
-                    {x:0, y: 123},
-                    {x:1, y: 100},
-                    {x:2, y: 83},
-                    {x:3, y: 185},
+        datapoints: [
+          { x: 0, y: 123 },
+          { x: 1, y: 100 },
+          { x: 2, y: 83 },
+          { x: 3, y: 185 },
           { x: 4, y: 12 },
-        ],
-            },
-      l2: {
-                datapoints: [
-                    {x:0, y: 45},
-                    {x:1, y: 72},
-                    {x:2, y: 6},
-                    {x:3, y: 111},
-          { x: 4, y: 12 },
-        ],
-            },
-      l3: {
-                datapoints: [
-                    {x:0, y: 1.32},
-                    {x:1, y: 24.123242},
-                    {x:2, y: 62.12},
-                    {x:3, y: 0.432},
-                    {x:4, y: 10},
         ],
       },
-        };
+      l2: {
+        datapoints: [
+          { x: 0, y: 45 },
+          { x: 1, y: 72 },
+          { x: 2, y: 6 },
+          { x: 3, y: 111 },
+          { x: 4, y: 12 },
+        ],
+      },
+      l3: {
+        datapoints: [
+          { x: 0, y: 1.32 },
+          { x: 1, y: 24.123242 },
+          { x: 2, y: 62.12 },
+          { x: 3, y: 0.432 },
+          { x: 4, y: 10 },
+        ],
+      },
+    };
 
-        for ( let i = 0; i < numbersToChange; i++ ) {
-            const whichArray = Math.floor((Math.random() * 3) + 1);
+    for (let i = 0; i < numbersToChange; i++) {
+      const whichArray = Math.floor((Math.random() * 3) + 1);
 
-            let range = [10, 150];
+      let range = [10, 150];
 
-            if (whichArray === 3) {
-                range = [0, 75];
-            }
+      if (whichArray === 3) {
+        range = [0, 75];
+      }
 
-            const newNum = (Math.random() * range[1]) + range[0];
-            const whichIndex = Math.floor((Math.random() * 5));
+      const newNum = (Math.random() * range[1]) + range[0];
+      const whichIndex = Math.floor((Math.random() * 5));
 
       newData[`l${whichArray}`].datapoints[whichIndex].y = newNum;
-        }
-
-        newState.data = newData;
-        this.setState(newState);
     }
 
-    addPoint() {
-        const newState = this.state;
-        const newNum = (Math.random() * 180) + 5;
+    newState.data = newData;
+    this.setState(newState);
+  }
+
+  addPoint() {
+    const newState = this.state;
+    const newNum = (Math.random() * 180) + 5;
     newState.data.l2.datapoints.push({ x: newState.data.l2.datapoints.length, y: newNum });
-        this.setState(_.cloneDeep(newState));
-    }
+    this.setState(_.cloneDeep(newState));
+  }
 
-    setVisibility(val, key) {
-        const natures = this.state.natures;
+  setVisibility(val, key) {
+    const natures = this.state.natures;
 
     natures.forEach((n) => {
-            if (_.isArray(n.specs)){
+      if (_.isArray(n.specs)) {
         n.specs.forEach((s) => {
-                    if (s.getKey() == key){
-                        s.show = val;
-                    }
-                });
-      } else if (n.specs.getKey() == key) {
-                    n.specs.show = val;
-                }
+          if (s.getKey() == key) {
+            s.show = val;
+          }
         });
+      } else if (n.specs.getKey() == key) {
+        n.specs.show = val;
+      }
+    });
 
-        this.setState({...this.state, natures});
-    }
+    this.setState({ ...this.state, natures });
+  }
 
-    render(){
-        return (
-            <div>
-                <Header/>
-                <div className="chart-stack">
-                    <ChartBlock title={strings.charts.lineChart} data={this.state.data} setVisibility={(val, key) => this.setVisibility(val, key)}>
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="chart-stack">
+          <ChartBlock title={strings.charts.lineChart} data={this.state.data} setVisibility={(val, key) => this.setVisibility(val, key)}>
             <Chart domainPadding={5} padding={48} data={this.state.data} natures={this.state.natures} />
-                    </ChartBlock>
-                </div>
-                <div className="button" onClick={() => this.buttonClicked()}>Change Data</div>
-                <div className="button" onClick={() => this.addPoint()}>Add Point</div>
-            </div>
-        );
-    }
+          </ChartBlock>
+        </div>
+        <div className="button" onClick={() => this.buttonClicked()}>Change Data</div>
+        <div className="button" onClick={() => this.addPoint()}>Add Point</div>
+      </div>
+    );
+  }
 }
 
-render( <App/>, document.getElementById( 'app' ) );
+render(<App />, document.getElementById('app'));
